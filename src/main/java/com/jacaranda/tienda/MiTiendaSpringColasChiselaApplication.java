fd09f52c28ca84a9d3838079210ea59f13c4447f
chisela14 <chisela14@gmail.com>
2022-12-21 09:24:54 +0100

    first commit

---
+++ src/main/java/com/jacaranda/tienda/MiTiendaSpringColasChiselaApplication.java
@@ -0,0 +1,13 @@
+package com.jacaranda.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiTiendaSpringColasChiselaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiTiendaSpringColasChiselaApplication.class, args);
+	}
+
+}
