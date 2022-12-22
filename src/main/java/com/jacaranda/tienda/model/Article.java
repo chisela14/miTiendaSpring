3424f0a08e743d5eb4da027a87cc2ca230e088e7
chisela14 <chisela14@gmail.com>
2022-12-22 10:05:08 +0100

    inicio categoria

---
+++ src/main/java/com/jacaranda/tienda/model/Article.java
@@ -0,0 +1,102 @@
+package com.jacaranda.tienda.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="FLOWER")
+public class Article {
+
+		@Id
+		private int code;
+		private String name;
+		private String description;
+		private float price;
+		@ManyToOne 
+		@JoinColumn(name="color")
+		private Category color;
+		private int stock;
+//		@OneToMany(mappedBy= "flower", cascade= CascadeType.ALL, orphanRemoval = true)
+//		private List<Purchase> purchasesList = new ArrayList<>();
+		
+		public Article() {
+			
+		}
+
+		public Article(String name, String description, float price, Category color) {
+			this.name = name;
+			this.description = description;
+			this.price = price;
+			this.color = color;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public float getPrice() {
+			return price;
+		}
+
+		public void setPrice(float price) {
+			this.price = price;
+		}
+
+		public Color getColor() {
+			return color;
+		}
+
+		public void setColor(Color color) {
+			this.color = color;
+		}
+
+		public int getCode() {
+			return code;
+		}
+
+		public int getStock() {
+			return stock;
+		}
+
+		public void setStock(int stock) {
+			this.stock = stock;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(code);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Flower other = (Flower) obj;
+			return code == other.code;
+		}
+
+		@Override
+		public String toString() {
+			return code + "<br>" + name + "         " + color.getCode() + "<br>Descripción: " + description + "<br>" + price + " €";
+		}
+
+}
